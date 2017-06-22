d87098c1af20f71b483e246e7338f516a9a7d76c
Zakant <zakant@gmx.de>
2017-06-22 14:05:57 +0200

    Added intercept entry functions

---
+++ src/PythonManager.cpp
@@ -0,0 +1,13 @@
+
+#include "PythonManager.hpp"
+
+namespace sqf = intercept::sqf;
+namespace client = intercept::client;
+
+void __cdecl intercept::pre_init() {
+
+}
+
+void __cdecl intercept::pre_start() {
+
+}
--- src/PythonManager.hpp
+++ src/PythonManager.hpp
@@ -2,3 +2,7 @@
 
 #include "intercept.hpp"
 #include "boost/python.hpp"
+
+void __cdecl intercept::pre_init();
+
+void __cdecl intercept::pre_start();
