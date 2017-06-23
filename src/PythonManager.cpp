3dda216d604d20411903da017ebc9eb584cf686d
Zakant <zakant@gmx.de>
2017-06-23 13:16:09 +0200

    Sample code

--- src/PythonManager.cpp
+++ src/PythonManager.cpp
@@ -3,11 +3,17 @@
 
 namespace sqf = intercept::sqf;
 namespace client = intercept::client;
+namespace python = boost::python;
 
 void __cdecl intercept::pre_init() {
 
 }
 
 void __cdecl intercept::pre_start() {
+	Py_Initialize();
+	python::object main_module = python::import("__main__");
+	python::object main_namespace = main_module.attr("__dict__");
 
+	main_namespace["systemChat"] = &sqf::system_chat;
+	python::exec("systemChat('Hello from Pythonland')", main_namespace);
 }
