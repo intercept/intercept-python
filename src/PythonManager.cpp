eb6e6e48f0b7d7d77ef8324728ef99ae9261c2de
Zakant <zakant@gmx.de>
2017-06-23 14:06:23 +0200

    Moved code to on_frame

--- src/PythonManager.cpp
+++ src/PythonManager.cpp
@@ -5,15 +5,22 @@ namespace sqf = intercept::sqf;
 namespace client = intercept::client;
 namespace python = boost::python;
 
+python::object main_module;
+python::object main_namespace;
+
 void __cdecl intercept::pre_init() {
 
 }
 
 void __cdecl intercept::pre_start() {
+	sqf::system_chat("Intercept Python was loaded!");
 	Py_Initialize();
-	python::object main_module = python::import("__main__");
-	python::object main_namespace = main_module.attr("__dict__");
-
+	main_module = python::import("__main__");
+	main_namespace = main_module.attr("__dict__");
 	main_namespace["systemChat"] = &sqf::system_chat;
+	
+}
+
+void __cdecl intercept::on_frame() {
 	python::exec("systemChat('Hello from Pythonland')", main_namespace);
 }
