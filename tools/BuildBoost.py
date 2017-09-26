0f5d4d86824501b72da040d3439694d654db534f
Lukasz Taczuk <overfl0@users.noreply.github.com>
2017-09-27 01:26:51 +0200

    Style changes: - use "not in" construct - no parentheses in ifs - improved readability

--- tools/BuildBoost.py
+++ tools/BuildBoost.py
@@ -11,16 +11,17 @@ compileString = 'bjam --user-config=autobuild-config.jam --with-python python-de
 
 def main(argv):
     print("Building Boost.Python")
-    if (not 'BOOST_ROOT' in os.environ):
+    if 'BOOST_ROOT' not in os.environ:
         print("Cant find boost. Set 'BOOST_ROOT' enviroment variable to your boost root!")
         return
     else:
         boostPath = os.environ['BOOST_ROOT']
+
     print("Boost path is: {}".format(boostPath))
     os.chdir(boostPath)
+
     # Creating output folder
-    if not os.path.exists('lib'):
-        os.makedirs('lib')
+    os.makedirs('lib', exist_ok=True)
 
     # Checking if bjam is present. Run bootstrap otherwise!
     if not os.path.exists('bjam.exe'):
@@ -29,14 +30,14 @@ def main(argv):
         os.system("bootstrap.bat")
 
     print("Searching for python")
-    build64 = not '--no64' in argv
-    build86 = not '--no86' in argv
+    build64 = '--no64' not in argv
+    build86 = '--no86' not in argv
 
-    buildDebug = not '--noDebug' in argv
-    buildRelease = not '--noRelease' in argv
+    buildDebug = '--noDebug' not in argv
+    buildRelease = '--noRelease' not in argv
 
-    if (build64):
-        if ('PYTHON_PATH_64' in os.environ):
+    if build64:
+        if 'PYTHON_PATH_64' in os.environ:
             python_x64 = os.environ['PYTHON_PATH_64']
             print("Found Python x64: {}".format(python_x64))
         else:
@@ -44,8 +45,9 @@ def main(argv):
             return
     else:
         print("Disabled x64 building")
-    if (build86):
-        if ('PYTHON_PATH_86' in os.environ):
+
+    if build86:
+        if 'PYTHON_PATH_86' in os.environ:
             python_x86 = os.environ['PYTHON_PATH_86']
             print("Found Python x86: {}".format(python_x86))
         else:
@@ -54,10 +56,11 @@ def main(argv):
     else:
         print("Disabled x86 building")
 
-    if (build64):
+    if build64:
         print("Building Boost.Python x64")
         build(boostPath, python_x64, 64, buildDebug, buildRelease)
-    if (build86):
+
+    if build86:
         print("Building Boost.Python x86")
         build(boostPath, python_x86, 32, buildDebug, buildRelease)
 
@@ -81,24 +84,26 @@ def build(boostPath, pythonPath, architecture, buildDebug, buildRelease):
                                          os.path.join(pythonPath, 'include').replace('\\', '\\\\'),
                                          os.path.join(pythonPath, 'libs').replace('\\', '\\\\')))
 
-    if (buildDebug):
+    if buildDebug:
         print("Building Debug")
         os.system(compileString.format('shared', 'debug', str(architecture)))
         os.system(compileString.format('static', 'debug', str(architecture)))
-    if (buildRelease):
+    if buildRelease:
         print("Building Release")
         os.system(compileString.format('shared', 'release', str(architecture)))
         os.system(compileString.format('static', 'release', str(architecture)))
 
     # Clean	up output
     outputFolder = os.path.join('lib', 'win{}'.format(str(architecture)))
-    if (os.path.exists(outputFolder)):
+    if os.path.exists(outputFolder):
         shutil.rmtree(outputFolder)
     os.makedirs(outputFolder)
+
     # Copy results into ouput
     for f in glob.glob(r'./stage/lib/*'):
         print("Copying from {} to {}".format(f, os.path.join(outputFolder, ntpath.basename(f))))
         shutil.copy(f, os.path.join(outputFolder, ntpath.basename(f)))
+
     # And copy all libs without `python3`
     for f in glob.glob(r'./stage/lib/*.lib'):
         shutil.copy(f, os.path.join(outputFolder, ntpath.basename(f).replace('python3', 'python')))
